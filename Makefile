32bd547600884f2c16a140bbaaa00673ea10538e
Plouf Plouf <plouf@plouf.fr>
2020-11-16 18:38:52 +0100

    oubli correction makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-run: main.gp mylib.so
+run: main.gp mylib.so primes.bin
 	cat main.gp |gp -q
 	
 mylib.so: mylib.c
@@ -6,5 +6,12 @@ mylib.so: mylib.c
 
 clean:
 	rm -f *~ *.so *.o
+
+primes.bin: one-billion-primes.txt parse
+	./parse < one-billion-primes.txt 2> primes.bin
+
+parse: parse.c
+	cc -o parse parse.c
+
 
 .PHONY: run clean
--- parse.c
+++ parse.c
@@ -7,7 +7,7 @@ int main(void) {
     while (fgets(buf, 1024, stdin) != NULL) {
         nread++;
         if (!(nread % 1000000 )) {
-            printf("%u %% loaded\n", nread / 1000000);
+            printf("%u %% converted\n", nread / 1000000);
         }
         unsigned long long primes[10];
         sscanf(buf, "%llu %llu %llu %llu %llu %llu %llu %llu %llu %llu", primes, primes + 1, primes + 2, primes + 3, primes + 4, primes + 5, primes + 6, primes + 7, primes + 8, primes + 9);
